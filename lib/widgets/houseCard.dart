318f8647ba83659bb81cd693dc817e4f1ecdf99c
malksh <malksh009@gmail.com>
2023-05-10 16:17:12 +0200

    last

--- lib/widgets/houseCard.dart
+++ lib/widgets/houseCard.dart
@@ -92,140 +92,143 @@ class _HouseCerdState extends State<HouseCerd> {
   ];
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 300,
-      margin: const EdgeInsets.only(right: 20),
-      decoration: BoxDecoration(
-        color: const Color(0xfcf9f8),
-        borderRadius: BorderRadius.circular(8),
-        border: Border.all(color: Colors.grey.shade200),
-      ),
-      child: GestureDetector(
-        onTap: widget.onTap,
-        child: Card(
-          child: Padding(
-            padding: const EdgeInsets.all(12),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Container(
-                  width: double.infinity,
-                  height: 130,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(8),
-                    color: Colors.grey.shade200,
-                    image: DecorationImage(
-                      image: NetworkImage(widget.item.thumb_url!),
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    Text(
-                      widget.item.catagory ?? '',
-                      style: const TextStyle(
-                        color: Colors.blue,
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
+    return Directionality(
+      textDirection: TextDirection.ltr,
+      child: Container(
+        width: 300,
+        margin: const EdgeInsets.only(right: 20),
+        decoration: BoxDecoration(
+          color: const Color(0xfcf9f8),
+          borderRadius: BorderRadius.circular(8),
+          border: Border.all(color: Colors.grey.shade200),
+        ),
+        child: GestureDetector(
+          onTap: widget.onTap,
+          child: Card(
+            child: Padding(
+              padding: const EdgeInsets.all(12),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    width: double.infinity,
+                    height: 130,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8),
+                      color: Colors.grey.shade200,
+                      image: DecorationImage(
+                        image: NetworkImage(widget.item.thumb_url!),
+                        fit: BoxFit.cover,
                       ),
                     ),
-                  ],
-                ),
-                Text(
-                  widget.item.title!,
-                  style: const TextStyle(
-                    color: Colors.blue,
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
                   ),
-                  overflow: TextOverflow.ellipsis,
-                ),
-                Row(
-                  children: [
-                    const Icon(
-                      Icons.location_on,
-                      color: Colors.grey,
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Text(
+                        widget.item.catagory ?? '',
+                        style: const TextStyle(
+                          color: Colors.blue,
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Text(
+                    widget.item.title!,
+                    style: const TextStyle(
+                      color: Colors.blue,
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
                     ),
-                    Text(
-                      widget.item.location!,
-                      style: const TextStyle(
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  Row(
+                    children: [
+                      const Icon(
+                        Icons.location_on,
                         color: Colors.grey,
-                        fontSize: 16,
-                        fontWeight: FontWeight.normal,
                       ),
-                      overflow: TextOverflow.ellipsis,
-                    ),
-                  ],
-                ),
-                Row(
-                  children: [
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          '${widget.item.price}د.ل / شهري',
-                          textAlign: TextAlign.end,
-                          style: const TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 15,
-                          ),
-                          overflow: TextOverflow.ellipsis,
+                      Text(
+                        widget.item.location!,
+                        style: const TextStyle(
+                          color: Colors.grey,
+                          fontSize: 16,
+                          fontWeight: FontWeight.normal,
                         ),
-                        Text(
-                          '${widget.item.phoneNumber}',
-                          style: const TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 15,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            '${widget.item.price}د.ل / شهري',
+                            textAlign: TextAlign.end,
+                            style: const TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 15,
+                            ),
+                            overflow: TextOverflow.ellipsis,
                           ),
-                          overflow: TextOverflow.ellipsis,
-                        ),
-                      ],
-                    ),
-                    IconButton(
-                      color: widget.item.color! ? Colors.red : Colors.black,
-                      icon: const Icon(Icons.favorite_outlined),
-                      onPressed: () {
-                        setState(() {
-                          isFavorite = !isFavorite;
-                          if (allOfTheBulding[widget.index].color!) {
-                            try {
-                              print('00000000000000000000');
-                              print(widget.item.color!);
-
-                              widget.item.color = !widget.item.color!;
-                              allOfTheBulding[widget.index].color =
-                                  !widget.item.color!;
-                            } on Exception catch (e) {
-                              // TODO
+                          Text(
+                            '${widget.item.phoneNumber}',
+                            style: const TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 15,
+                            ),
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                        ],
+                      ),
+                      IconButton(
+                        color: widget.item.color! ? Colors.red : Colors.black,
+                        icon: const Icon(Icons.favorite_outlined),
+                        onPressed: () {
+                          setState(() {
+                            isFavorite = !isFavorite;
+                            if (allOfTheBulding[widget.index].color!) {
+                              try {
+                                print('00000000000000000000');
+                                print(widget.item.color!);
+    
+                                widget.item.color = !widget.item.color!;
+                                allOfTheBulding[widget.index].color =
+                                    !widget.item.color!;
+                              } on Exception catch (e) {
+                                // TODO
+                              }
+                            } else {
+                              try {
+                                print('1111111111111111111111');
+                                print(widget.item.color!);
+    
+                                widget.item.color = !widget.item.color!;
+                                allOfTheBulding[widget.index].color =
+                                    !widget.item.color!;
+                              } on Exception catch (e) {
+                                print("---------------------------${e}");
+                              }
                             }
+                          });
+    
+                          if (!allOfTheBulding[widget.index].color!) {
+                            favorite.add(widget.item);
                           } else {
-                            try {
-                              print('1111111111111111111111');
-                              print(widget.item.color!);
-
-                              widget.item.color = !widget.item.color!;
-                              allOfTheBulding[widget.index].color =
-                                  !widget.item.color!;
-                            } on Exception catch (e) {
-                              print("---------------------------${e}");
-                            }
+                            favorite.remove(widget.item);
+                            setState(() {});
                           }
-                        });
-
-                        if (!allOfTheBulding[widget.index].color!) {
-                          favorite.add(widget.item);
-                        } else {
-                          favorite.remove(widget.item);
-                          setState(() {});
-                        }
-                      },
-                    ),
-                  ],
-                ),
-              ],
+                        },
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         ),
--- lib/widgets/selectCatagory.dart
+++ lib/widgets/selectCatagory.dart
@@ -5,14 +5,12 @@ import 'package:flutter/material.dart';
 import '../models/ItemModel.dart';
 
 class SelectCatagory extends StatefulWidget {
-  const SelectCatagory({super.key});
+  const SelectCatagory({Key? key}) : super(key: key);
 
   @override
   State<SelectCatagory> createState() => _SelectCatagoryState();
 }
- late final Item item;
-  late final int index;
-  late final Function()? onTap;
+
 class _SelectCatagoryState extends State<SelectCatagory> {
   @override
   Widget build(BuildContext context) {
@@ -22,17 +20,17 @@ class _SelectCatagoryState extends State<SelectCatagory> {
       child: ListView(
         scrollDirection: Axis.horizontal,
         children: [
-          catagoryButton(Icons.house_rounded, 'منازل', context),
-          catagoryButton(Icons.villa_rounded, 'مكاتب', context),
-          catagoryButton(Icons.apartment_rounded, 'شقق', context),
-          catagoryButton(Icons.castle_rounded, 'مخازن', context),
+          catagoryButton(Icons.house_rounded, 'منازل', context, 0),
+          catagoryButton(Icons.villa_rounded, 'مكاتب', context, 1),
+          catagoryButton(Icons.apartment_rounded, 'شقق', context, 2),
+          catagoryButton(Icons.castle_rounded, 'مخازن', context, 3),
         ],
       ),
     );
   }
 }
 
-Widget catagoryButton(IconData icon, String? text, context) {
+Widget catagoryButton(IconData icon, String? text, BuildContext context, int index) {
   return Container(
     margin: const EdgeInsets.all(18),
     // padding: const EdgeInsets.all(8),
@@ -43,9 +41,9 @@ Widget catagoryButton(IconData icon, String? text, context) {
     ),
     child: InkWell(
       onTap: () {
-         Navigator.push(context,
-                    // ignore: prefer_const_constructors
-                    MaterialPageRoute(builder: (context) =>  HousesCatagory(text: 'text', item: item, index: index)));
+        Navigator.push(context, MaterialPageRoute(builder: (context) {
+          return HousesCatagory(index: index, item: Item(color: null, location: '', phoneNumber: null, price: null, thumb_url: '', title: '', type: ''), text: text!,);
+        }));
       },
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
